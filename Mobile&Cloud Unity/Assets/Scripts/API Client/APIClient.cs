865915f8fcd926c376439792be68753ff3646e85
Alexandre Alvarez Messias <ale03.alvarez@hotmail.com>
2017-10-25 21:56:46 -0200

    Servidor funcionando e código para pegar itens pronto

--- Mobile&Cloud Unity/Assets/Scripts/API Client/APIClient.cs
+++ Mobile&Cloud Unity/Assets/Scripts/API Client/APIClient.cs
@@ -4,11 +4,11 @@ using UnityEngine.Networking;
 
 public class APIClient : MonoBehaviour
 {
-    private const string baseUrl = "http://localhost:54583/API";
+    private const string baseUrl = "http://localhost:54853/API";
 
 	void Start ()
     {
-		
+        StartCoroutine("GetItensAPISync");
 	}
 
     IEnumerator GetItensAPISync()
@@ -25,6 +25,24 @@ public class APIClient : MonoBehaviour
         {
             string response = request.downloadHandler.text;
             Debug.Log(response);
+
+            Item[] itens = JSonHelper.getJsonArray<Item>(response);
+
+            foreach (Item i in itens)
+            {
+                ImprimirItem(i);
+            }
         }
+    }
+
+    private void ImprimirItem(Item i)
+    {
+        Debug.Log("=========== Dados do Objeto ===========");
+        Debug.Log("ID: " + i.ItemID);
+        Debug.Log("Nome: " + i.Nome);
+        Debug.Log("Descrição: " + i.Descricao);
+        Debug.Log("Dano Máximo: " + i.DanoMaximo);
+        Debug.Log("Raridade: " + i.Raridade);
+        Debug.Log("Tipo Item ID: " + i.Raridade);
     }
 }
