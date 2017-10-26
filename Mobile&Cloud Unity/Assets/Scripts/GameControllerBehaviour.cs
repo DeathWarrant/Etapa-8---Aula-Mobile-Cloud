6527964fa17097ddc420dbd1ea580c43f0316c12
Alexandre Alvarez Messias <ale03.alvarez@hotmail.com>
2017-10-25 22:22:10 -0200

    Novas funcionalidades

---
+++ Mobile&Cloud Unity/Assets/Scripts/GameControllerBehaviour.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameControllerBehaviour : MonoBehaviour
+{
+    public GameObject inventario = null;
+    public Image[] slots = null;
+    public Sprite[] itemSprites = null;
+
+    private bool isInventarioOnCooldown = false;
+    private float inventarioCooldown = 0.0f;
+
+	void Start ()
+    {
+        InitializeComponents();
+	}
+	
+	void Update ()
+    {
+        GetInputs();
+        ManageStuff();
+	}
+
+    private void InitializeComponents()
+    {
+        inventario.SetActive(false);
+        isInventarioOnCooldown = false;
+    }
+
+    private void GetInputs()
+    {
+        if (Input.GetAxisRaw("Abrir Inventário") != 0.0f && !isInventarioOnCooldown && !inventario.activeInHierarchy)
+        {
+            inventario.SetActive(true);
+            isInventarioOnCooldown = true;
+        }
+        else if (Input.GetAxisRaw("Abrir Inventário") != 0.0f && !isInventarioOnCooldown && inventario.activeInHierarchy)
+        {
+            inventario.SetActive(false);
+            isInventarioOnCooldown = true;
+        }
+    }
+
+    private void ManageStuff()
+    {
+        if(isInventarioOnCooldown)
+        {
+            inventarioCooldown += Time.deltaTime;
+
+            if(inventarioCooldown >= 0.5f)
+            {
+                isInventarioOnCooldown = false;
+            }
+        }
+
+
+    }
+}
